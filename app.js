a7aae6f1ce1b9e196cc5e990cbf505abb7a00c18
Clement Murphy <clemmurphy@gmail.com>
2021-10-30 14:05:26 +0100

    Created custom SoundCloud player

--- app.js
+++ app.js
@@ -1,26 +1,60 @@
-
 window.addEventListener('DOMContentLoaded', () => {
 
   const urlInput = document.getElementById('url-input')
-  const getButton = document.getElementById('get-button')
-  const bodyContent = document.getElementById('body-content')
+  let url = 'https://soundcloud.com/officialbyrontheaquarius/chop-crew-pt1'
 
-  let url = ''
-  urlInput.addEventListener('input', () => {
-    url = urlInput.value
+  const artistName = document.getElementById('artist-name')
+  const trackName = document.getElementById('track-name')
+
+  const iframeWidget = SC.Widget('player-iframe')
+  const playerArtwork = document.getElementById('player-artwork')
+  const playPauseButton = document.getElementById('play-pause')
+  const scLink = document.getElementById('sc-link')
+
+  playPauseButton.addEventListener('click', () => {
+    if (playPauseButton.dataset.playing === '1') {
+      iframeWidget.pause()
+      playPauseButton.dataset.playing = '0'
+      playPauseButton.innerText = 'Play'
+      playPauseButton.classList.remove('pause-button')
+      playPauseButton.classList.add('play-button')
+    } else {
+      iframeWidget.play()
+      playPauseButton.dataset.playing = '1'
+      playPauseButton.innerText = 'Pause'
+      playPauseButton.classList.remove('play-button')
+      playPauseButton.classList.add('pause-button')
+    }
+    console.log('Play button clicked')
   })
-  
-  getButton.addEventListener('click', () => {
+
+  const getTrackData = () => {
     fetch(`https://soundcloud.com/oembed?format=json&url=${url}`)
       .then(
         function(response){
           const res = response.json()
           res.then((data) => {
             console.log(data)
-            bodyContent.innerHTML = data.html
+            playerArtwork.src = data.thumbnail_url
+            artistName.innerText = data.author_name
+            trackName.innerText = data.title
+            scLink.addEventListener('click', () => {
+              window.open(
+                data.author_url,
+                '_blank'
+              )
+            })
           })
         }
       )
+  }
+
+  getTrackData()
+
+  urlInput.addEventListener('input', () => {
+    url = urlInput.value
+    getTrackData()
   })
+
 })
 
